6ccb8d18a3b942aae980524c0e1b1ebd66b4fa29
Darchi <frolova.madina@gmail.com>
2024-10-23 16:40:33 +0300

    AddServices

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,37 +1,54 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(CollisionHandler))]
+[RequireComponent(typeof(InputHandler))]
 [RequireComponent(typeof(Health))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private CollisionHandler _collisionHandler;
 
+    private InputHandler _inputHandler;
     private Health _health;
 
     private Transform _hand;
+    private bool _hasBall = false;
 
     private void Awake()
     {
         _collisionHandler = GetComponent<CollisionHandler>();
         _health = GetComponent<Health>();
+        _inputHandler = GetComponent<InputHandler>();
     }
 
     private void OnEnable()
     {
         _collisionHandler.CollisionDetected += Interact;
         _health.Died += RaiseDeath;
+        _inputHandler.Clicked += OnClicked;
     }
 
     private void OnDisable()
     {
         _collisionHandler.CollisionDetected -= Interact;
         _health.Died -= RaiseDeath;
+        _inputHandler.Clicked -= OnClicked;
     }
 
     public void TakeDamage(float amount)
     {
         _health.TakeDamage(amount);
+    }
+
+    private void OnClicked()
+    {
+        if (_hasBall)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 
     private void Interact(IInteractable interactable)
